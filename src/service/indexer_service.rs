506d4ae80c9d8e6f137a3a251358f4e3e917e796
rishabh-vasudevan <rishabhvasudevan19@gmail.com>
2023-07-15 23:31:10 +0530

    added parallel processing to improve efficiency

--- src/service/indexer_service.rs
+++ src/service/indexer_service.rs
@@ -1,11 +1,27 @@
 use axum::Extension;
+use futures;
 use sqlx::PgPool;
+use std::future::Future;
 use std::error::Error;
-use std::hash::Hash;
+use std::thread::sleep;
+use std::time::Duration;
 use std::{collections::HashMap, time::Instant};
 
 use crate::controller;
 use crate::utils::{constants, util_functions};
+
+async fn join_parallel<T: Send + 'static>(
+    futs: impl IntoIterator<Item = impl Future<Output = T> + Send + 'static>,
+) -> Vec<T> {
+    let tasks: Vec<_> = futs.into_iter().map(tokio::spawn).collect();
+    // unwrap the Result because it is introduced by tokio::spawn()
+    // and isn't something our caller can handle
+    futures::future::join_all(tasks)
+        .await
+        .into_iter()
+        .map(Result::unwrap)
+        .collect()
+}
 
 pub async fn run_indexer_impl(pool: Extension<PgPool>) -> Result<(), Box<dyn Error>> {
     //find the current epoch head go from (ep_head-6, ep_head -1)
@@ -23,35 +39,49 @@ pub async fn run_indexer_impl(pool: Extension<PgPool>) -> Result<(), Box<dyn Err
             util_functions::find_committee_and_validators_for_epoch(epoch).await;
         let mut committee_attestation_bits_for_epoch_mapping: HashMap<(i64, String), Vec<bool>> =
             HashMap::new();
-        for slot in ((epoch) * constants::NUMBER_OF_SLOTS_PER_EPOCH)
-            ..((epoch + 1) * constants::NUMBER_OF_SLOTS_PER_EPOCH)
-        {
-            let committee_attestation_bits_mapping: (
-                bool,
-                Option<HashMap<(i64, String), Vec<bool>>>,
-            ) = util_functions::find_committee_attestations_bits_mapping(epoch, slot).await;
 
-            if committee_attestation_bits_mapping.0 {
-                match committee_attestation_bits_mapping.1 {
-                    Some(val) => {
-                        committee_attestation_bits_for_epoch_mapping.extend(val);
-                    },
-                    None => ()
-                }
-            } else {
-                continue;
+        let range = ((epoch as u64) * constants::NUMBER_OF_SLOTS_PER_EPOCH as u64)
+            ..((epoch as u64) * constants::NUMBER_OF_SLOTS_PER_EPOCH as u64 + 16);
+        let stream: Vec<(bool, Option<HashMap<(i64, String), Vec<bool>>>)> =
+            join_parallel(range.into_iter().map(|slot| async move {
+                util_functions::find_committee_attestations_bits_mapping(epoch.clone(), slot as i64)
+                    .await
+            }))
+            .await;
+
+        for attestation_in_slot in &stream {
+            if let Some(val) = &attestation_in_slot.1 {
+                committee_attestation_bits_for_epoch_mapping.extend(val.to_owned());
             }
         }
 
+        sleep(Duration::from_secs(1));
+
+        let range2 = ((epoch as u64) * constants::NUMBER_OF_SLOTS_PER_EPOCH as u64 + 16)
+            ..((epoch as u64 + 1) * constants::NUMBER_OF_SLOTS_PER_EPOCH as u64);
+        let stream2: Vec<(bool, Option<HashMap<(i64, String), Vec<bool>>>)> =
+            join_parallel(range2.into_iter().map(|slot| async move {
+                util_functions::find_committee_attestations_bits_mapping(epoch.clone(), slot as i64)
+                    .await
+            }))
+            .await;
+
+        for attestation_in_slot in &stream2 {
+            if let Some(val) = &attestation_in_slot.1 {
+                committee_attestation_bits_for_epoch_mapping.extend(val.to_owned());
+            }
+        }
+
+        sleep(Duration::from_secs(1));
         if committee_attestation_bits_for_epoch_mapping.len() > 0 {
-                    util_functions::write_attestation_data_to_postgres(
-                        &committee_validators_mapping,
-                        committee_attestation_bits_for_epoch_mapping,
-                        epoch,
-                        &pool,
-                    )
-                    .await;
-                }
+            util_functions::write_attestation_data_to_postgres(
+                &committee_validators_mapping,
+                committee_attestation_bits_for_epoch_mapping,
+                epoch,
+                &pool,
+            )
+            .await;
+        }
     }
 
     println!("run_indexer_impl :: it took {:?} ", start_time.elapsed());
